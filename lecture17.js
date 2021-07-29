3d7e202e27fd2c922b956bcd62a77b59e6ba908e
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-07-29 16:13:00 +0530

    Javascript tutorial  | Lecture: 17 loops in js, for loops, with explanation and examples, finished

---
+++ lecture17.js
@@ -0,0 +1,11 @@
+//for loop examples
+
+console.log("Hey for loop");
+/*suppose you want to print this Hey for loop 1000 times so what you do, First you copy the this console.log() 1000 times,
+which consuming lot of effort and time. so the second option are,
+using for() loop 
+*/
+
+for(let i = 0; i <= 10; i++) {
+    console.log(`hey ${i}`);
+}
