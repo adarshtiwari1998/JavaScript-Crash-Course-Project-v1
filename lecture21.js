2a89c6e8ac809d8cc994e86df1fd69e32278d5ff
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-07-30 13:59:03 +0530

    Javascript tutorial  | Lecture: 21, DOM object and manipulation with explanation and examples, finished

---
+++ lecture21.js
@@ -0,0 +1,17 @@
+//dom manupulation example 
+console.log("hey");
+
+console.log(document.title)  //Lecture 21 | Dom Manipulation in javascript |
+
+
+console.log(document.head)   //output <head></head> part print
+
+console.log(document.body)    //output <body></body> part print
+
+
+
+//change the title of the page dynamically without refershing the  page
+
+console.log(document.title="Lecture 21 | DOM object manupulation");  //output: title change to Lecture 21 | DOM object manupulation
+
+//hum number of things kar sakte hai DOM se
