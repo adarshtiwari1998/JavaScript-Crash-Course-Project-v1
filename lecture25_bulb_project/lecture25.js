e078d2de19483c624372aefa18fb44d0a4053afc
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-08-06 23:52:10 +0530

    Javascript Tutorial | Lecture: 25, javascript project 1 , lecture 25.js file, top comment section content added and updatd, finished

--- lecture25_bulb_project/lecture25.js
+++ lecture25_bulb_project/lecture25.js
@@ -1,10 +1,10 @@
-/* 2 function use hoga turnOn function and turnOff function */
+/* 2 function use hoga turnOn function and turnOff function, and aik toggle function for default on and off button */
 //TurnOn Function()
 const turnOnFun = () => {
-   //turnOn bulb with the help of set attribute function
-   let turnOnImgObj = document.getElementById("bulbControl");
-   let turnOnRes = turnOnImgObj.setAttribute("src", "./assets/images/on_bulb.jpg");
-   console.log(turnOnRes);
+    //turnOn bulb with the help of set attribute function
+    let turnOnImgObj = document.getElementById("bulbControl");
+    let turnOnRes = turnOnImgObj.setAttribute("src", "./assets/images/on_bulb.jpg");
+    console.log(turnOnRes);
 }
 
 //turn off function()
@@ -18,25 +18,25 @@ const turnOffFun = () => {
 //turnOnOff function
 
 const turnOnOffFun = () => {
-       let turnOnImgObj = document.getElementById("bulbControl");
-       let attSrcValue = turnOnImgObj.getAttribute("src");
-       console.log(attSrcValue);  
-        /* current state bta dega aapko ki src ki
-        value kya hai abhi off hai on hai, getAttribute() function */
-        /* humko button ka state bhi nikalna padega and then value change karna padega */
-        let buttonObj = document.getElementById("onOffButon");
-        let buttonStateFind = buttonObj.innerHTML;
-        console.log(buttonStateFind);
-       //if condition apply 
-       if(attSrcValue == "./assets/images/off_bulb.jpg") {
-             //call the turn on function here
-            turnOnFun();
-            buttonObj.innerHTML = "Off";
-            // console.log(turnOnFun);
-        } else {
-            turnOffFun();
-            buttonObj.innerHTML = "On";
-            // console.log(turnOffFun);
-        }
-} 
+    let turnOnImgObj = document.getElementById("bulbControl");
+    let attSrcValue = turnOnImgObj.getAttribute("src");
+    console.log(attSrcValue);
+    /* current state bta dega aapko ki src ki
+    value kya hai abhi off hai on hai, getAttribute() function */
+    /* humko button ka state bhi nikalna padega and then value change karna padega */
+    let buttonObj = document.getElementById("onOffButon");
+    let buttonStateFind = buttonObj.innerHTML;
+    console.log(buttonStateFind);
+    //if condition apply 
+    if (attSrcValue == "./assets/images/off_bulb.jpg") {
+        //call the turn on function here
+        turnOnFun();
+        buttonObj.innerHTML = "Off";
+        // console.log(turnOnFun);
+    } else {
+        turnOffFun();
+        buttonObj.innerHTML = "On";
+        // console.log(turnOffFun);
+    }
+}
 
