555998fc2c90a12389293c294e5e706793e20515
adarshtiwari1998 <tiwari1998adarsh@gmail.com>
2021-08-10 15:06:18 +0530

    Javascript Tutorial | Lecture 29 and 30( part 2), digital clock with am/pm, start mode in h,m,s,  with explanation and exmaple, finished

--- lecture_29_Digital_Clock_Project/lecture29.js
+++ lecture_29_Digital_Clock_Project/lecture29.js
@@ -3,18 +3,31 @@ const getDateTime = () => {
 
     //get current Date  and Time
     let getDateTimeObj = new Date();
-    console.log(getDateTimeObj);
+    // console.log(getDateTimeObj);
       //Tue Aug 10 2021 13:52:04 GMT+0530 (India Standard Time)
 //humko ab meaningful cheeze nikalni hai like hour, second
-   let hours = getDateTimeObj.getHours();
-   console.log(hours); //output: 13
-   let minute = getDateTimeObj.getMinutes();
-   console.log(minute);  //output: 56
-   let second = getDateTimeObj.getSeconds();
-//    let miliSecond = getDateTimeObj.getMilliseconds();
-   console.log(second); //output: 17
 
-   return `${hours} : ${minute} : ${second} `;
+   let hours = getDateTimeObj.getHours();
+    //console.log(hours); //output: 13
+    //new variable for AM/PM using ternary operator
+   let amPm = hours>=12? "PM" : "AM";
+    //console.log(amPm); //output:
+    /* start mode in hours*/
+   hours = hours % 12;
+   hours = hours==0?12: hours;
+   hours = hours > 12 ? hours: "0" + hours;
+   let minutes = getDateTimeObj.getMinutes();
+   //console.log(minute);  //output: 56
+    minutes = minutes > 9 ? minutes: "0" + minutes;
+   let seconds = getDateTimeObj.getSeconds();
+   //console.log(second); //output: 17
+   seconds = seconds > 9 ? seconds: "0" + seconds;
+   let milliSeconds = getDateTimeObj.getMilliseconds();
+
+   return `${hours} : ${minutes} : ${seconds} : ${milliSeconds} : ${amPm} `;
+
+ /* Second Method by using localTimeString method */
+//return getDateTimeObj.toLocaleTimeString();
 
 }
 
